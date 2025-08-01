3e25768c71736dbba42ef04fabfef774c4ecdf80
c0der4t <mekron@ekronds.co.za>
2025-08-01 05:20:19 +0200

    Played with the UI a bit. Got some cool stuff going.

--- app/src/main/java/vcnmb/hannah/currencyconverter/MainActivity.kt
+++ app/src/main/java/vcnmb/hannah/currencyconverter/MainActivity.kt
@@ -1,6 +1,9 @@
 package vcnmb.hannah.currencyconverter
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var cmbxSrcCountr = findViewById<Spinner>(R.id.spnSourceCountry)
+        val classList = mutableListOf("Far Far Away")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, classList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        cmbxSrcCountr.adapter = adapter
+
+        classList.add("The Swamp")
+        adapter.notifyDataSetChanged()
+
     }
 }
